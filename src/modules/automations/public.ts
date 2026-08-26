2c8bced4f0356c59d19cc8cfb21d1a0dc91adb8a
Frederico mourao <fredmourao@gmail.com>
2026-08-26 19:48:44 -0300

    feat: add safe scheduled automations
    CI and Database passed.

--- src/modules/automations/public.ts
+++ src/modules/automations/public.ts
@@ -1,1 +1,4 @@
 export { scheduleAppointmentConfirmations } from './application/schedule-appointment-confirmations'
+export { runDailyAutomations } from './application/run-daily-automations'
+export { sendBirthdayGreetings } from './application/send-birthday-greetings'
+export { createCancellationDeadlineReminders } from './application/create-cancellation-deadline-reminders'
